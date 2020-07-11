ae93de7ad5150ea88638175d2b655cbfa83f958f
peter.a.cowen <peter.a.cowen@gmail.com>
2020-07-11 19:14:36 +0100

    app.config and ;pg fi;e

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace LogFileManagement
 {
+    using System.Configuration;
+    using System.IO;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! How are you?");
+            string fileName = ConfigurationManager.AppSettings["LogFiles"];
+            Console.WriteLine("Hello World! How are you?"+fileName);
+
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine(File.ReadAllText(fileName));
+            } else
+            {
+                Console.WriteLine("file doesn't exist");
+            }
         }
     }
 }
