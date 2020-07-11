2137eff24f102a2d91fc5bf5bd68417ecf1b017f
peter.a.cowen <peter.a.cowen@gmail.com>
2020-07-11 14:40:55 +0100

    Add new project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogFileManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
