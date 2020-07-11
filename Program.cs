5392e6ba73b3a8df741dd55165760860a863bf18
peter.a.cowen <peter.a.cowen@gmail.com>
2020-07-11 23:22:31 +0100

    find files and check age

--- Program.cs
+++ Program.cs
@@ -4,20 +4,57 @@ namespace LogFileManagement
 {
     using System.Configuration;
     using System.IO;
+    using Newtonsoft.Json.Linq;
 
     class Program
     {
         static void Main(string[] args)
         {
+            
             string fileName = ConfigurationManager.AppSettings["LogFiles"];
-            Console.WriteLine("Hello World! How are you?"+fileName);
 
-            if (File.Exists(fileName))
+            if (fileName is null)
             {
-                Console.WriteLine(File.ReadAllText(fileName));
+                Console.WriteLine("LogFiles is not configured in AppSettings");
+                System.Environment.Exit(-1);
+            }
+           
+            if (File.Exists(fileName) == false)
+            {
+                Console.WriteLine("AppSettings.LogFiles {0} does not exit", fileName);
+                System.Environment.Exit(-1);
+            }
+
+
+            JArray logFileArray = JArray.Parse(File.ReadAllText(fileName));
+
+            foreach (var logFile in logFileArray)
+            {
+                string folder = logFile["Folder"].Value<string>();
+                string filePattern = logFile["FilePattern"].Value<string>();
+                int retentionPeriodDays = logFile["RetentionPeriodDays"].Value<int>();
+
+                ManageFolderRetention(folder, filePattern, retentionPeriodDays);
+            }
+            
+        }
+
+        private static void ManageFolderRetention(string folder, string filePattern, int retentionPeriodDays)
+        {
+            if (Directory.Exists(folder) == false)
+            {
+                Console.WriteLine("Log folder {0} does not exist", folder);
             } else
             {
-                Console.WriteLine("file doesn't exist");
+                Console.WriteLine("Folder {0}, pattern {1}\n", folder, filePattern);
+                foreach (string file in Directory.EnumerateFiles(folder, filePattern)){
+                    FileInfo fi = new FileInfo(file);
+                    if (fi.CreationTime < DateTime.Now.AddDays(-retentionPeriodDays))
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+                
             }
         }
     }
