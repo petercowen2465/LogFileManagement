2108787ea6fe13d4fe67f8cba31385273db2ceee
peter.a.cowen <peter.a.cowen@gmail.com>
2020-07-11 14:46:17 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace LogFileManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! How are you?");
         }
     }
 }
